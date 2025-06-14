4a37737da18751b689d78de2ae9ab87acf8d78d9
Asmita <brahmankarasmita99@gmail.com>
2025-06-14 18:46:15 +0530

    Second new class are created

---
+++ src/SecondClass.java
@@ -0,0 +1,9 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 78;
+        int c = a / b;
+        System.out.println(c);
+
+    }
+}
